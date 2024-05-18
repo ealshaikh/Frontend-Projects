cbdb0fc04855159ac30190d6aa94c6ae034277d1
Esraa <eamahmoud198@cit.just.edu.jo>
2024-05-18 20:10:19 +0300

    Initial Commit

--- Navbar_Toggle/script.js
+++
@@ -1,6 +0,0 @@
-const iocnBtn = document.querySelector('.nav-toggle');
-const links = document.querySelector(".links");
-
-iocnBtn.addEventListener("click", function () {
-    links.classList.toggle("show-links");
-  });
